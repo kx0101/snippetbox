757b0c34db1723f67371048f0f408131acb2cfba
kx0101 <liakos.koulaxis@yahoo.com>
2024-07-17 18:55:26 +0300

    .

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"letsgobook/internal/models"
+	"letsgobook/internal/validator"
 	"net/http"
 	"strconv"
 	"strings"
@@ -15,6 +16,7 @@ type snippetCreateForm struct {
 	Content     string
 	Expires     int
 	FieldErrors map[string]string
+	validator.Validator
 }
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -77,27 +79,17 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 	}
 
 	form := snippetCreateForm{
-		Title:       r.PostForm.Get("title"),
-		Content:     r.PostForm.Get("content"),
-		Expires:     expires,
-		FieldErrors: map[string]string{},
+		Title:   r.PostForm.Get("title"),
+		Content: r.PostForm.Get("content"),
+		Expires: expires,
 	}
 
-	if strings.TrimSpace(form.Title) == "" {
-		form.FieldErrors["title"] = "The field cannot be empty"
-	} else if utf8.RuneCountInString(form.Title) > 100 {
-		form.FieldErrors["title"] = "The field cannot be more than 100 characters long"
-	}
+	form.CheckField(validator.Notblank(form.Title), "title", "This field cannot be empty")
+	form.CheckField(validator.MaxChars(form.Title, 100), "title", "The field cannot be more than 100 characters long")
+	form.CheckField(validator.Notblank(form.Content), "content", "The field cannot be empty")
+	form.CheckField(validator.PermittedInt(form.Expires, 1, 7, 365), "expires", "The field must equal to 1, 7 or 365")
 
-	if strings.TrimSpace(form.Content) == "" {
-		form.FieldErrors["content"] = "The field cannot be empty"
-	}
-
-	if form.Expires != 1 && form.Expires != 7 && form.Expires != 365 {
-		form.FieldErrors["expires"] = "The field must equal to 1, 7 or 365"
-	}
-
-	if len(form.FieldErrors) > 0 {
+	if !form.Valid() {
 		data := app.newTemplateData(r)
 		data.Form = form
 		app.render(w, http.StatusUnprocessableEntity, "create.tmpl", data)
