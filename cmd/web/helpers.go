fd96259b81cc63bfaf879b63b58866578b161ae7
kx0101 <liakos.koulaxis@yahoo.com>
2024-07-23 21:58:33 +0300

    .

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -76,7 +76,6 @@ func (app *application) decodePostForm(r *http.Request, dst any) error {
 }
 
 func (app *application) isAuthenticated(r *http.Request) bool {
-	// return app.sessionManager.Exists(r.Context(), "authenticatedUserID")
 	isAuthenticated, ok := r.Context().Value(isAuthenticatedContextKey).(bool)
 
 	if !ok {
