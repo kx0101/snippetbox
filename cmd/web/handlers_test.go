91021b6933c1c0b55f6d9ce3f8c550fe0b8554d0
kx0101 <liakos.koulaxis@yahoo.com>
2024-07-27 08:14:17 +0300

    .

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -17,3 +17,61 @@ func TestPing(t *testing.T) {
 	assert.Equal(t, code, http.StatusOK)
 	assert.Equal(t, body, "OK")
 }
+
+func TestSnippetView(t *testing.T) {
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.route())
+	defer ts.Close()
+
+	tests := []struct {
+		name     string
+		urlPath  string
+		wantCode int
+		wantBody string
+	}{
+		{
+			name:     "Valid ID",
+			urlPath:  "/snippet/view/1",
+			wantCode: http.StatusOK,
+			wantBody: "An old silent pond...",
+		},
+		{
+			name:     "Non-existent ID",
+			urlPath:  "/snippet/view/2",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "Negative ID",
+			urlPath:  "/snippet/view/-1",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "Decimal ID",
+			urlPath:  "/snippet/view/1.23",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "String ID",
+			urlPath:  "/snippet/view/foo",
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "Empty ID",
+			urlPath:  "/snippet/view/",
+			wantCode: http.StatusNotFound,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			code, _, body := ts.get(t, tt.urlPath)
+
+			assert.Equal(t, code, tt.wantCode)
+
+			if tt.wantBody != "" {
+				assert.StringContains(t, body, tt.wantBody)
+			}
+		})
+	}
+}
--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -1,11 +1,22 @@
 package assert
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func Equal[T comparable](t *testing.T, actual, expected T) {
 	t.Helper()
 
 	if actual != expected {
 		t.Errorf("got %v; expected %v", actual, expected)
+	}
+}
+
+func StringContains(t *testing.T, actual, expected string) {
+	t.Helper()
+
+	if !strings.Contains(actual, expected) {
+		t.Errorf("got %v; expected to contains %v", actual, expected)
 	}
 }
